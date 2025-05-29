7d6af741aca7be3eb77bcda11b2fe58ac88b985b
Chenrui <2021111664@stu.sufe.edu.cn>
2025-05-29 15:39:53 +0800

    add additional experiments for MNIST and Fashion-MNIST

--- run.sh
+++ run.sh
@@ -8,6 +8,36 @@ python mnist.py \
     --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
     # --force-rerun \
 
+python mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source 42 \
+    --target 23333 \
+    --max-iter 500 \
+    --tol 1e-6 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+    # --force-rerun \
+
+python mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source 1926 \
+    --target 817 \
+    --max-iter 500 \
+    --tol 1e-6 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+    # --force-rerun \
+
+python mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source 1234 \
+    --target 5678 \
+    --max-iter 500 \
+    --tol 1e-6 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+    # --force-rerun \
+
 python fashion_mnist.py \
     --reg 0.001 \
     --norm l1 \
@@ -17,6 +47,37 @@ python fashion_mnist.py \
     --tol 1e-6 \
     --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
     # --force-rerun \
+
+python fashion_mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source 42 \
+    --target 23333 \
+    --max-iter 500 \
+    --tol 1e-6 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+    # --force-rerun \
+
+python fashion_mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source 1926 \
+    --target 817 \
+    --max-iter 500 \
+    --tol 1e-6 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+    # --force-rerun \
+
+python fashion_mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source 1234 \
+    --target 5678 \
+    --max-iter 500 \
+    --tol 1e-6 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+    # --force-rerun \
+
 
 python imagenette.py \
     --reg 0.001 \
