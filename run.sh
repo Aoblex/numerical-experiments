119c34d9616353f6258fb9f6ea5a2d780b461e08
Chenrui <2021111664@stu.sufe.edu.cn>
2025-05-30 13:47:36 +0800

    update run.sh to match experiments in the paper

--- run.sh
+++ run.sh
@@ -1,106 +1,132 @@
-python mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 2 \
-    --target 54698 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
+#!/bin/bash
 
-python mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 42 \
-    --target 23333 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-python mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 1926 \
-    --target 817 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-python mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 1234 \
-    --target 5678 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-python fashion_mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 2 \
-    --target 54698 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-python fashion_mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 42 \
-    --target 23333 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-python fashion_mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 1926 \
-    --target 817 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-python fashion_mnist.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 1234 \
-    --target 5678 \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
-
-
-python imagenette.py \
-    --reg 0.001 \
-    --norm l1 \
-    --source 'tench' \
-    --target 'cassette player' \
-    --max-iter 500 \
-    --tol 1e-6 \
-    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
+####################
+# synthetic data I #
+####################
 
 python synthetic1.py \
     --reg 0.001 \
     --size 1000 \
-    --max-iter 500 \
-    --tol 1e-6 \
     --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
+
+python synthetic1.py \
+    --reg 0.001 \
+    --size 5000 10000 \
+    --methods BCD LBFGS-Dual SSNS SPLR
+
+
+#####################
+# synthetic data II #
+#####################
 
 python synthetic2.py \
     --reg 0.001 \
     --size 1000 \
-    --max-iter 500 \
-    --tol 1e-6 \
     --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
-    # --force-rerun \
+
+python synthetic2.py \
+    --reg 0.001 \
+    --size 5000 10000 \
+    --methods BCD LBFGS-Dual SSNS SPLR
+
+
+#########
+# MNIST #
+#########
+
+python mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source     2   239 17390 34860 \
+    --target 54698 43981 49947 45815 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+
+#################
+# Fashion-MNIST #
+#################
+
+python fashion_mnist.py \
+    --reg 0.001 \
+    --norm l1 \
+    --source     2   239 17390 34860 \
+    --target 54698 43981 49947 45815 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+############
+# ImageNet #
+############
+
+python imagenette.py \
+    --reg 0.01 0.001 \
+    --norm l1 l2 \
+    --source 'tench' \
+    --target 'cassette player' \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+
+##################
+# Ablation study #
+##################
+
+python synthetic1.py \
+    --reg 0.001 \
+    --size 1000 5000 10000 \
+    --methods SPLR 'Sparse Newton' \
+    --force-rerun
+
+python synthetic2.py \
+    --reg 0.001 \
+    --size 1000 5000 10000 \
+    --methods SPLR 'Sparse Newton' \
+    --force-rerun
+
+####################################
+# Extra test examples (eta = 0.01) #
+####################################
+
+# synthetic data I
+python synthetic1.py \
+    --reg 0.01 \
+    --size 1000 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+python synthetic1.py \
+    --reg 0.01 \
+    --size 5000 10000 \
+    --methods BCD LBFGS-Dual SSNS SPLR
+
+# synthetic data II
+python synthetic2.py \
+    --reg 0.01 \
+    --size 1000 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+python synthetic2.py \
+    --reg 0.01 \
+    --size 5000 10000 \
+    --methods BCD LBFGS-Dual SSNS SPLR
+
+# MNIST
+python mnist.py \
+    --reg 0.01 \
+    --norm l1 \
+    --source     2   239 17390 34860 \
+    --target 54698 43981 49947 45815 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+# Fashion-MNIST
+python fashion_mnist.py \
+    --reg 0.01 \
+    --norm l1 \
+    --source     2   239 17390 34860 \
+    --target 54698 43981 49947 45815 \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
+
+# ImageNet
+python imagenette.py \
+    --reg 0.01 0.001 \
+    --norm l1 l2 \
+    --source 'tench'           'tench'  'tench'         'tench' \
+    --target 'cassette player' 'church' 'garbage truck' 'golf ball' \
+    --methods BCD APDAGD LBFGS-Dual Newton SSNS SPLR
